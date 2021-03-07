d40c304c8d4c7d0e294fba2ab754b5a3189bae65
Alex <oleksandrvojtenko@yahoo.com>
2021-03-07 10:31:45 +0200

    Work with FileWriter, FileReader, Inheritance and Interfaces

---
+++ src/main/java/Lesson_branch_8/Admin.java
@@ -0,0 +1,11 @@
+package Lesson_branch_8;
+
+public class Admin extends Person {
+
+    private String role;
+
+    public Admin(String firstName, String secondName, int age, String email, String password) {
+        super(firstName, secondName, age, email, password);
+        this.role = "ADMIN";
+    }
+}
---
+++ src/main/java/Lesson_branch_8/Service.java
@@ -0,0 +1,11 @@
+package Lesson_branch_8;
+
+import java.io.IOException;
+
+public interface Service {
+
+    public boolean checkUser(Person person) throws Exception;
+
+    public void writeData(Person person) throws Exception;
+
+}
